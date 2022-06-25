eb87347f2753a68e133603524c51b61522dfbc5e
Veronika <vvnika@mail.ru>
2022-06-25 19:25:23 +1100

    add "Seminar_1"

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,4 +1,6 @@
-﻿int a = -9;
+﻿int Max(int a, int b) => a > b ? a : b;
+int Max(int a, int b, int c) => Max(Max(a, b), c);
+int a = -9;
 int b = -3;
- 
+int c = -9;
 Console.WriteLine((a > b) ? "{0} {1}" : "{1} {0}", a, b);
